c19d0d5a727f44adac5fbb13b13c5b915455ddbd
Aloysius Lim <aloysiusl@singtel.com>
2018-01-19 13:56:07 +0800

    Added shapefiles.

--- server.R
+++ server.R
@@ -8,9 +8,31 @@
 #
 
 library(leaflet)
+library(memoise)
+library(rgdal)
 library(shiny)
 
-# Define server logic
+
+#### Parameters
+roi.layer <- "sub-zone"
+shp.meta <- data.frame(
+  layer = c("MP14_REGION_WEB_PL", "MP14_PLNG_AREA_WEB_PL", "MP14_SUBZONE_WEB_PL"),
+  govId.name = c("REGION_C", "PLN_AREA_C", "SUBZONE_C"),
+  row.names = c("planning-region", "planning-area", "sub-zone"),
+  stringsAsFactors = FALSE
+)
+
+
+#### Helpers
+
+# Get ROI data from Shapefiles
+get.roishape <- memoise(function(roiLayer) {
+  readOGR("shapefiles", shp.meta[roiLayer, "layer"]) %>%
+    spTransform(CRS("+ellps=WGS84 +proj=longlat +datum=WGS84 +no_defs"))
+})
+
+
+#### Server Logic
 server <- function(input, output) {
 
   # Logo
@@ -23,11 +45,22 @@ server <- function(input, output) {
     format(Sys.time(), "%Y-%m-%d %H:%M:%S %Z")
   })
 
-  # Map
+  # Base map
   output$map <- renderLeaflet({
     leaflet() %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       setView(103.82, 1.34, 11)
+  })
+
+  # ROI shapes
+  rois <- reactive({ get.roishape(roi.layer) })
+
+  # ROI layer with data
+  observe({
+    rois <- rois()
+    leafletProxy("map", data = rois) %>%
+      clearGroup("rois") %>%
+      addPolygons(weight = 1, fillOpacity = 0.7, group = "rois")
   })
 
 }
