ad86913ac494573d89eca397487e411ba2ef0906
Aloysius Lim <aloysiusl@singtel.com>
2018-01-30 10:32:33 +0800

    Added tooltips and highlighting to map.

--- server.R
+++ server.R
@@ -9,6 +9,7 @@
 
 library(dplyr)
 library(dygraphs)
+library(htmltools)
 library(httr)
 library(leaflet)
 library(memoise)
@@ -35,17 +36,19 @@ timestamp.refresh.ms <- 10000
 default.tz <- "Asia/Singapore"
 shp.meta <- data.frame(
   layer = c("MP14_REGION_WEB_PL", "MP14_PLNG_AREA_WEB_PL", "MP14_SUBZONE_WEB_PL"),
-  govId.name = c("REGION_C", "PLN_AREA_C", "SUBZONE_C"),
-  govName.name = c("REGION_N", "PLN_AREA_N", "SUBZONE_N"),
-  levelType.name = c("planningregion", "planningarea", "subzone"),
+  gov.id = c("REGION_C", "PLN_AREA_C", "SUBZONE_C"),
+  gov.name = c("REGION_N", "PLN_AREA_N", "SUBZONE_N"),
+  roi.id = c("planningregion", "planningarea", "subzone"),
+  roi.name = c("planningregion_name", "planningarea_name", "subzone_name"),
   row.names = c("planning-region", "planning-area", "sub-zone"),
   stringsAsFactors = FALSE
 )
 
 
 #### Default (empty results)
-default.footfall <- data.frame(timestamp = as.POSIXct(Sys.time()), roi = "N.A.", status = "N.A.",
-                               count = 0L)
+default.footfall <- data.frame(timestamp = as.POSIXct(Sys.time()), roi.id = "N.A.",
+                               roi.name = "N.A.", status = "N.A.", count = 0L,
+                               stringsAsFactors = FALSE)
 
 
 #### Helpers
@@ -94,7 +97,7 @@ get.footfall <- function(planning.region, roi.layer, token, interval = 10) {
     location = list(locationType = "locationHierarchyLevel", levelType = "planningregion",
                     id = planning.region),
     interval = interval,
-    dimensionFacets = c("status", shp.meta[roi.layer, "levelType.name"]),
+    dimensionFacets = c("status", shp.meta[roi.layer, "roi.id"], shp.meta[roi.layer, "roi.name"]),
     aggregations = list(list(metric = "total_stays", type = "longSum"))
   )
   log.query(query.body, "Footfall")
@@ -107,7 +110,8 @@ get.footfall <- function(planning.region, roi.layer, token, interval = 10) {
       gather_array %>%
       spread_values(
         timestamp.string = jstring("timestamp"),
-        roi = jstring("event", shp.meta[roi.layer, "levelType.name"]),
+        roi.id = jstring("event", shp.meta[roi.layer, "roi.id"]),
+        roi.name = jstring("event", shp.meta[roi.layer, "roi.name"]),
         status = jstring("event", "status"),
         count = jstring("event", "longSum_total_stays")
       ) %>%
@@ -115,7 +119,7 @@ get.footfall <- function(planning.region, roi.layer, token, interval = 10) {
         timestamp = as.POSIXct(timestamp.string, format = "%Y-%m-%dT%H:%M:%S", tz = default.tz),
         count = as.integer(count)
       ) %>%
-      select(timestamp, roi, status, count)
+      select(timestamp, roi.id, roi.name, status, count)
     result
   } else {
     default.footfall
@@ -134,6 +138,7 @@ server <- function(input, output, session) {
     key.secret.b64 <- base64_encode(paste(cred$key, cred$secret, sep = ":"))
   })
 
+  # Access token
   token <- reactive({
     token.response <- POST(token.api.endpoint,
                            add_headers(Authorization = paste("Basic", creds.b64())),
@@ -177,8 +182,8 @@ server <- function(input, output, session) {
   # ROI shapes
   roi <- reactive({
     r <- get.roishape(input$layer)
-    r@data$roi <- r@data[[ shp.meta[input$layer, "govId.name"] ]]
-    r@data$roi.name <- r@data[[ shp.meta[input$layer, "govName.name"] ]]
+    r@data$roi.id <- as.character(r@data[[ shp.meta[input$layer, "gov.id"] ]])
+    r@data$roi.name <- as.character(r@data[[ shp.meta[input$layer, "gov.name"] ]])
     r
   })
 
@@ -187,17 +192,17 @@ server <- function(input, output, session) {
     timestamp.refresh.ms, session, latest.ts,
     function() {
       r <- roi()@data
-      planning.regions <- as.character(unique(r[[ shp.meta["planning-region", "govId.name"] ]]))
+      planning.regions <- as.character(unique(r[[ shp.meta["planning-region", "gov.id"] ]]))
       ff <- bind_rows(lapply(planning.regions, get.footfall, input$layer, token()))
       ff[ff$timestamp == max(ff$timestamp), ]
     })
 
-  # Footfall map data
+  # Footfall map data, filtered by selected status
   current.footfall.map <- reactive({
     ff <- current.footfall()
     if (input$status == "All") {
       ff %>%
-        group_by(roi) %>%
+        group_by(roi.id, roi.name) %>%
         summarise(
           timestamp = max(timestamp),
           status = "All",
@@ -209,12 +214,12 @@ server <- function(input, output, session) {
   })
 
   # Palette for shading current footfall
-  palette <- reactive({
+  pal <- reactive({
     ff <- current.footfall.map()
     if (nrow(ff) <= 1) {
-      colorNumeric("OrRd", 0)
+      colorBin("OrRd", 0, bins = 1)
     } else {
-      colorNumeric("OrRd", ff$count)
+      colorBin("OrRd", ff$count, bins = 7)
     }
   })
 
@@ -226,11 +231,18 @@ server <- function(input, output, session) {
   observe({
     r <- roi()
     ff <- current.footfall.map()
-    r@data <- left_join(r@data, ff, "roi")
+    r@data <- left_join(r@data, ff, c("roi.id", "roi.name"))
+    labels <- sprintf("<strong>%s (%s)</strong><br/>%s: %d",
+                      r@data$roi.name, r@data$roi.id, r@data$status, r@data$count) %>%
+      lapply(HTML)
     leafletProxy("map", data = r) %>%
       clearGroup("rois") %>%
-      addPolygons(weight = 1, fillColor = ~palette()(count), fillOpacity = 0.7, group = "rois",
-                  layerId = ~roi)
+      addPolygons(weight = 1, color = "royalblue", fillColor = ~pal()(count), fillOpacity = 0.7,
+                  group = "rois", layerId = ~roi.id,
+                  highlight = highlightOptions(weight = 5, color = "royalblue4", bringToFront = TRUE),
+                  label = labels,
+                  labelOptions = labelOptions(style = list("font-weight" = "normal"), direction = "auto")
+                  )
   })
 
   # Time series chart
